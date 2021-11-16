22704a15831d8cd1b80a02aae1ac19aae25289c6
Vivien Timmers <djyak.be@gmail.com>
2021-11-16 21:43:51 +1100

    added show, create, starting the edit part

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,36 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :create, :edit]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.edit(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks, only: [:index, :show, :new, :create, :destroy, :edit, :update]
+  # get 'tasks', to: 'tasks#index', as: :tasks
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # post 'tasks/edit', to: 'tasks#edit', as: :edit_task
+  # destroy 'task'
 end
